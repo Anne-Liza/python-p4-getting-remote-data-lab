416914b5690b1c9cd8d64dec095e0ad8c3cead24
Anne Liza Wangui <annewangui210@gmail.com>
2024-10-11 16:20:01 +0300

    Passed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.content
+        else:
+            print(f"Error: Received status code {response.status_code}")
+            return None
 
     def load_json(self):
-        pass
+        body = self.get_response_body()
+        if body is not None:
+            return json.loads(body)
+        else:
+            return None
